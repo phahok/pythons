01cb845eda62d466db94d3e585fe861ef1b52601
Jose Renato Ferreira <joserenatosp@outlook.com>
2022-04-07 14:33:43 -0300

    Update pytube_import_youtube_video.py

--- youtube/pytube_import_youtube_video.py
+++ youtube/pytube_import_youtube_video.py
@@ -1,7 +1,8 @@
 from pytube import YouTube
 from pytube.cli import on_progress
 
-print("Baixe e converta videos Youtube \n By José Renato Ferreira\n")
+print("##YOUTUBE DOWNLOADS ##\n\nBaixe e converta videos Youtube \nBy José Renato Ferreira\n\n")
+
 link = input("Insira o link do video: ")
 
 yt = YouTube(link, on_progress_callback = on_progress)
