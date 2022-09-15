6bb016d2ca1a12a47dc9741805f1e28becf777c2
Jose Renato Ferreira <joserenatosp@outlook.com>
2022-09-15 18:30:07 +0000

    update code python resizing

--- imgs_crop/resizing_img.py
+++ imgs_crop/resizing_img.py
@@ -3,9 +3,9 @@ import os
 import os.path
 from PIL import Image
 
-f = r'/home/mobaxterm/pythons/imgs_crop/pics'
+f = r'/workspace/pythons/imgs_crop/pics'
 for file in os.listdir(f):
 	f_img = f+"/"+file
 	img = Image.open(f_img)
-	img = img.resize((2296,1724))
+	img = img.resize((550,420))
 	img.save(f_img)
